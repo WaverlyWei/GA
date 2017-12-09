a0b47d4406fd993ab3aaa4b80fbf2edeb3221d87
mohammadsp <mohammad@airbears2-10-142-92-37.airbears2.1918.berkeley.edu>
2017-12-08 16:20:19 -0800

    select.R

--- R/Select.R
+++ R/Select.R
@@ -4,24 +4,38 @@
 # Input: dataset, model, fitness function
 
 select <- function(dataset, model, fitness){
-
+  
   # Initialize the first generation
-  parents <- initialize(dataset)
-
-  convergenceFactor = 0.1 # what should be the convergence criterion?
-
-  (while cvg > convergenceFactor){
-
+  parents <- initiation(dataset)
+  
+  convergenceCriterion = 10e-8 # what should be the convergence criterion?
+  
+  while (cvg > convergenceCriterion){
+    
     #select and breed
-    children <- select(parents)
-
-    # should we do a for loop here over children or in the crossover and mutation functions?
-    # perform crossover and mutation on children
-    children <- crossover(children)
-    children <- mutation(children)
-
+    children <- selection(parents)
+    
+    # oldAIC = 
+    
+    # crossover
+    for (i in length(children)){
+      tmp = crossover(children[i,],children[i+1,])
+      children[i,] = tmp$P3
+      children[i+1,] = tmp$P4
+      i+1
+    }
+    
+    # mutation
+    children <- apply(children,1,mutation)
+    
+    # cvg = min(AIC(lm(,children))) - oldAIC
+    
+    # New parents
     parents <- children
-    # calculate cvg
+  
   }
-
+  
+  # Returning the selected variables
+  return(dataset[1,which(children[1,]==1)])
+  
 }
