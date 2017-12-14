dffa005cb704367e626f8b2b505be1ad401cf542
Mary Combs <marycombs@Marys-MacBook-Air.local>
2017-12-13 17:12:39 -0800

    Update all.R with model formula parameter

--- R/all.R
+++ R/all.R
@@ -1,135 +1,144 @@
 initData <- matrix( rnorm( 5000 , sd = 1:5 ) , ncol = 10 , byrow = TRUE )
 initOutcome <-1 + -1 * initData[ , 1 ] + 2 * initData[ , 3 ] + 1.1 * initData[ , 5 ] + 1.2 * initData[ , 7 ]
-data <- data.frame( initData , initOutcome )
+dataSet <- data.frame( initData , initOutcome )
 
+
+# fitness function option as defnied in givens_hoeting
 GHFitness <- function( P ){ 2 / P / ( P + 1 ) * seq( 1:P ) }
 
-calculateAIC <- function(data, outcome, parents, intercept){
-  # make X, Y
-  X <- data[ , !names( data ) %in% outcome ]
-  Y <- data[ , names( data ) %in% outcome ]
+initiation <- function( C , P ){
 
-  # initialize empty vector for AIC
-  P = dim( parents )[ 1 ]
-  AIC <- rep( 0 , P )
+  init_parents <- matrix( NA , nrow = P , ncol = C )
+  prob <- runif( P , min = 0.1 , max = 0.9 )
+  for( i in 1:P ){
+    init_parents[ i , ] <- sample( c( 0 , 1 ) , C , replace = TRUE , c( 1 - prob[ i ] , prob[ i ] ) )
+  }
 
-  # loop through each parent and regression with selected variables, and output AIC
-  for ( i in 1:P ){
-    if ( intercept[ i ] == 1 ){
-      AIC[ i ] <- AIC( lm( Y ~ . , data = data.frame( Y , X[ , which( parents[ i , ] == 1 ) ] ) ) )
-    } else {
-      AIC[ i ] <- AIC( lm( Y ~ 0 + . , data = data.frame( Y , X[ , which( parents[ i , ] == 1 ) ] ) ) )
+  return( init_parents )
+}
+
+selection <- function( mm , model , parents, P , fitness = GHFitness ){
+
+  parents <- subset( parents , rowSums( parents ) > 0 )
+  if ( dim( parents )[ 1 ] < P ){
+    for ( i in ( dim( parents )[ 1 ] + 1 ):P ){
+      parents <- rbind( parents , parents[ i - 1 , ] )
     }
   }
-  return(AIC)
-}
 
-Initiation <- function( data , pSize=length(data[,1]) , minC=0.1 , maxC=0.9 ){
+  # initialize empty vector for AIC
+  AIC <- rep( 0 , P )
 
-  vars <- length( data[ 1 , ] ) - 1
-  initMatrix <- matrix( data = NA , nrow = pSize , ncol = vars )
-  prob <- runif( pSize , min = minC , max = maxC )
-  for( i in 1:pSize )
-    initMatrix[ i , ] <- sample( c( 0 , 1 ) , vars , replace = TRUE , c( 1 - prob[ i ] , prob[ i ] ) )
-    intercept <- sample( c( 0 , 1 ) , pSize , replace = TRUE )
-
-  return( list( "InitMatrix" <- initMatrix , "Intercept" <- intercept ) )
-}
-
-selection <- function( data , outcome , parents , intercept , fitness = GHFitness ){
-
-  # determine number of parents in a population
-  P <- dim( parents )[ 1 ]
-
-  # check fitness function length
-  if( length( fitness( P ) ) != P ) { stop( "fitness function must output vector of length P" ) }
-
-  # run fitness function
-  fitness_prob <- fitness( P )
-
-  # check fitness function output is increasing in probability
-  if( !all.equal( order( fitness_prob ) , seq( 1:P ) ) ) { stop( "fitness function output must be increasing" ) }
-  if( sum( 0 < fitness_prob & fitness_prob < 1 ) != P ) { stop( "fitness function output must be probabilities (between 0 and 1)" ) }
-
-  AIC <- calculateAIC(data, outcome, parents, intercept)
+  for ( i in 1:P ){
+    dat <- data.frame( subset( mm , select = colnames( mm )[ which( parents[ i , ] == 1 ) ] ) )
+    mod <- reformulate( colnames( dat ) , model[[2]] )
+    AIC[ i ] <- AIC( lm( mod , data = dat ) )
+  }
 
   # assign fitness probabilities to calculated AICs, and select (stochastically) parents to keep
-  select_ind <- sample( order( AIC , decreasing = TRUE ) , P , prob = fitness_prob , replace = TRUE )
-  parents_selection <- parents[ select_ind , ]
-  intercept_selection <- intercept[ select_ind ]
+  select_ind <- sample( order( AIC , decreasing = TRUE ) , P , prob = fitness( P ) , replace = TRUE )
+  children <- parents[ select_ind , ]
 
-  return( list( parents_selection = parents_selection , intercept_selection = intercept_selection, AIC = AIC ) )
+  return( list( children = children , minAIC = min( AIC ) ) )
 }
 
-crossover <- function( P1 , P2 ){
-  n = length( P1 )
+crossover <- function( P1 , P2 , C ){
 
-  ##Choose crossover site randomly
-  site = floor( runif( 1 , min = 1 , max = n - 1 ) )
+  # Choose crossover site randomly
+  site = floor( runif( 1 , min = 1 , max = C - 1 ) )
 
-  ##Cut&Ligate parent strings
-  P3 = c( P1[ 1:site ] , P2[ ( site + 1 ):n ] )
-  P4 = c( P1[ ( site + 1 ):n ] , P2[ 1:site ] )
+  # Cut&Ligate parent strings
+  P3 = c( P1[ 1:site ] , P2[ ( site + 1 ):C ] )
+  P4 = c( P1[ ( site + 1 ):C ] , P2[ 1:site ] )
 
   ##Perhaps return all four strings??
   return( list( P3 = P3 , P4 = P4 ) )
 }
 
-mutation <-function( P , mutationProb ){
-
-  n = length( P )
+mutation <-function( P_i , mutationProb , C ){
 
   # Identifying which sites should be mutated
-  mut <- which( runif( n , 0 , 1 ) < mutationProb )
+  mut <- which( runif( C , 0 , 1 ) < mutationProb )
 
-  P[ mut ] <- ifelse( P[ mut ] == 1 , 0 , 1 )
-  return( P )
+  P_i[ mut ] <- ifelse( P_i[ mut ] == 1 , 0 , 1 )
+
+  return( P_i )
+
 }
 
-select <- function( data, model , fitness ){
+select <- function( data , model , conv_criterion = 10e-8 , steps = 50 ){
+
+  # data <- data.frame( data )
+
+  mm <- model.matrix( model , data = data )
+
+  # set C the length of each individual chromosome ,ie the number of vars
+  C <- length( attr( mm , "assign" ) )
+
+  # Randomly select P, the number of parents per generation, such that C < P < 2C with increasing probability for larger P
+  tmp <- seq( from = 2 * round( ( C + 1 ) / 2 ) , to = 2 * C , by = 2 )
+  P <- sample( tmp , 1 , prob = ( 2 / C ^ 2 ) * tmp )
 
   # Initialize the first generation
-  starting <- Initiation( data )
-  parents = starting[[1]]
-  intercept = starting[[2]]
+  init_parents <- initiation( C = C , P = P )
 
-  convergenceCriterion = 10e-8 # what should be the convergence criterion?
 
-  steps = 50
+  # initialize convergence vector
   convergence <- rep( 0 , steps )
 
+  # select fittest parents to breed
+  tmp <- selection( mm = mm , model = model , parents = init_parents , P = P )
+  parents <- tmp$children
+  parent_AIC <- tmp$minAIC
+
+  # print update
+  cat( "step 0" , colnames( mm )[ which( parents[ 1 , ] == 1 ) ] , "\n" , sep = " ")
+
+  # breed
   for ( i in 1:steps ){
 
-    #select and breed
-    tmp <- selection( data = data , outcome = "initOutcome" , parents , intercept)
-    children <- tmp [[1]]
-    intercept <- tmp [[2]]
-    oldAIC <- min(tmp[[3]])
+    # initialize children
+    children <- matrix( 0 , nrow = P , ncol = C )
 
     # crossover
-    for ( j in seq( from = 1 , to = length( children[ , 1 ] ) , by = 2 ) ){
-      tmp = crossover( children[ j , ] , children[ j + 1 , ] )
-      children[ j , ] = tmp$P3
-      children[ j + 1 , ] = tmp$P4
+    for ( j in seq( from = 1 , to = P , by = 2 ) ){
+
+      tmp <- crossover( parents[ j , ] , parents[ j + 1 , ] , C = C )
+      children[ j , ] <- tmp$P3
+      children[ j + 1 , ] <- tmp$P4
+
     }
 
     # mutation
-    children <- apply( children , 2 , mutation , mutationProb = 0.01 )
+    children <- apply( children , 2 , mutation , mutationProb = 0.01 , C = C )
 
-    newAIC <- min(calculateAIC(data, outcome = "initOutcome", children, intercept))
-    convergence[i] <- abs(newAIC - oldAIC)
+    # run regression with next generation
+    tmp <- selection( mm = mm , model = model , parents = children , P = P )
+    children <- tmp$children
+    children_AIC <- tmp$minAIC
+
+    # print update
+    cat( paste0( "step" , i ) , colnames( mm )[ which( children[ 1 , ] == 1 ) ] , "\n" , sep = " ")
+
+    # calculate convergenc criterion
+    convergence[ i ] <- abs(children_AIC - parent_AIC)
 
     # New parents
     parents <- children
+    parent_AIC <- children_AIC
 
   }
 
   # Returning the selected variables
-  return(list(which( children[ 1 , ] == 1), convergence))
+  return( list( which( children[ 1 , ] == 1 ), convergence ) )
 
 }
 
-GAresults = select(data)
+system.time( GAresults <- select( data = dataSet , model = initOutcome ~ X1:X2 + X1:X3 + X4 + X5 + X6 + X7 + X8 + X9 + X10 ) )
 variables = GAresults[[1]]
 convergence = GAresults[[2]]
-plot(convergence)
+variables
+convergence
+plot(convergence , pch = 16 , cex = 0.75 , xlab = "Step" , ylab = "Convergene Criterion")
+
+
